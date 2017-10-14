f09b0a5363b7da441ad19355a16e81c58fe0e9f8
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2017-10-14 20:07:38 +0200

    Fixed Alias Path

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {concatStr} from "../../core/utilities/utils";
+// import {concatStr} from "../../core/utilities/utils";
+import {concatStr} from "Utilities/utils";
 
 class Home extends Component {
   render() {
