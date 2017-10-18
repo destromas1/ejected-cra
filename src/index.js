438657fd18b6e656db224929eb8bd7e49af50fa3
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2017-10-18 13:08:14 +0200

    Routing Added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./app/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { store } from "./core/store/store.config";
+import SpeakerList from "./app/speakers/SpeakerList";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/speakers" component={SpeakerList} />
+      </Switch>
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
